8a5415b27b47cfb71f31188e82c45b270e817590
ShevinuM <nsmcooray@mun.ca>
2023-06-26 17:59:05 +0530

    update

--- src/main/java/com/example/demo/Application.java
+++ src/main/java/com/example/demo/Application.java
@@ -1,13 +1,28 @@
 package com.example.demo;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    CommandLineRunner commandLineRunner(StudentRepository studentRepository) {
+        return args -> {
+            Student shevinu = new Student(
+                    "Shevinu",
+                    "Nawalage",
+                    "shevinu.dev@gmail.com",
+                    21
+            );
+            studentRepository.save(shevinu);
+        };
     }
 
 }
